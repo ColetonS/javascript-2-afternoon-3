6a6511cd5a4910ad02b1ae0d33fe80e90a5da676
ColetonS <coleton.m.smith@gmail.com>
2019-06-27 08:56:29 -0600

    updates after jasmine was resolved

--- practice.js
+++ practice.js
@@ -95,8 +95,8 @@ multiply(4, 3, function(answer){
 */
 
 function contains(arr, name, cb) {
-  for (let i = 0; 0 < arr.length; i++) {
-    if (arr[i] = name) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == name) {
       cb(true)
     } 
   }
